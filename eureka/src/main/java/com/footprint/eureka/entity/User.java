7cc38bc507ebe2b97542e1810009c5588132d853
git <2917021186@qq.com>
2021-06-04 16:43:39 +0800

    fix:修复了用户模块的bug

--- eureka/src/main/java/com/footprint/eureka/entity/User.java
+++ eureka/src/main/java/com/footprint/eureka/entity/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.ibatis.type.Alias;
 
 import javax.validation.constraints.NotBlank;
 
--- user/src/main/java/com/footprint/user/dao/UserDao.java
+++ user/src/main/java/com/footprint/user/dao/UserDao.java
@@ -16,27 +16,27 @@ public interface UserDao {
      * @param user 用户Bean
      * @return 影响行数
      */
-    public int insertUser(@Param("user")User user);
+    public int insertUser(User user);
 
     /**
      * 根据条件查找用户
      * @param user
      * @return 查找到的用户列表
      */
-    public List<User> selectUsers(@Param("user")User user);
+    public List<User> selectUsers(User user);
 
     /**
      * 根据条件查询单个用户
      * @param user 用户Bean
      * @return 查找到的用户
      */
-    public User selectUser(@Param("user")User user);
+    public User selectUser(User user);
 
     /**
      * 统计指定条件用户的数量
      * @param user 用户 Bean
      * @return 数量
      */
-    public int selectCount(@Param("user")User user);
+    public int selectCount(User user);
 
 }
