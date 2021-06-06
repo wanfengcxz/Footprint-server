24b2ad1c6e441bde1622b32ba12b7144241af1ab
git <2917021186@qq.com>
2021-06-06 19:04:48 +0800

    Create Article.java

---
+++ eureka/src/main/java/com/footprint/eureka/entity/Article.java
@@ -0,0 +1,30 @@
+package com.footprint.eureka.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+
+    private int articleId;
+
+    private int userId;
+
+    private String title;
+
+    private String content;
+
+    private int totalLike;
+
+    private int timeStamp;
+
+    private int imageNum;
+
+    private int isLike;
+
+}
